fe4d208b715baa7081ca6bdd32cd1d40d458e743
Vik1ang <vik1ang@outlook.com>
2022-05-18 13:16:03 +0800

    init

---
+++ dart_study/04_string.dart
@@ -0,0 +1,16 @@
+main(List<String> args){
+  var str1 = 'abc';
+  var str2 = "abc";
+  var str3 = """
+    abc
+    cba
+  """;
+
+  // 2.
+  var name = "why";
+  var age = 19;
+  var height = 1.88;
+
+  var message = "my name is $name, age is $age, height is $height";
+  print(message);
+}
---
+++ dart_study/05_collections.dart
@@ -0,0 +1,9 @@
+main(List<String> args) {
+  var names = ["abc", "cba", "nba"];
+
+  var movies = {"a", "b"};
+
+  var info = {"name": "why", "age": 18};
+
+  
+}
