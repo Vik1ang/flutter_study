687582c74e86b1756f3138bd264c95c78deb10d6
Vik1ang <vik1ang@outlook.com>
2022-05-19 20:31:09 +0800

    flutter study

--- lib/main.dart
+++ lib/main.dart
@@ -17,40 +17,62 @@ class HYHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Flutter App"),
+          title: Center(
+            child: Text("商品列表"),
+          ),
         ),
-        body: HYContentBody());
+        body: HYHomeContent());
   }
 }
 
-class HYContentBody extends StatefulWidget {
+class HYHomeContent extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return HYContentBodyState();
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        HYHomeProductItem("Apple1", "Macbook1",
+            "https://img0.baidu.com/it/u=2698024750,2260552493&fm=253&fmt=auto&app=120&f=JPEG?w=641&h=401"),
+        HYHomeProductItem("Apple2", "Macbook2",
+            "https://img0.baidu.com/it/u=2698024750,2260552493&fm=253&fmt=auto&app=120&f=JPEG?w=641&h=401"),
+        HYHomeProductItem("Apple3", "Macbook3",
+            "https://img0.baidu.com/it/u=2698024750,2260552493&fm=253&fmt=auto&app=120&f=JPEG?w=641&h=401"),
+      ],
+    );
   }
 }
 
-class HYContentBodyState extends State<HYContentBody> {
-  var flag = true;
+class HYHomeProductItem extends StatelessWidget {
+  final String title;
+  final String desc;
+  final String imageURL;
+
+  final style1 = const TextStyle(fontSize: 25, color: Colors.orange);
+  final style2 = const TextStyle(fontSize: 20, color: Colors.green);
+
+  HYHomeProductItem(this.title, this.desc, this.imageURL);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Checkbox(
-            value: flag,
-            onChanged: (value) {
-              setState((){
-                flag = value!;
-              });
-            }),
-        const Text(
-          "同意协议",
-          style: TextStyle(fontSize: 20),
-        ),
-      ],
-    ));
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration:
+          BoxDecoration(border: Border.all(width: 5, color: Colors.black)),
+      child: Column(
+        children: <Widget>[
+          Text(
+            title,
+            style: style1,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(desc, style: style2),
+          const SizedBox(
+            height: 8,
+          ),
+          Image.network(imageURL)
+        ],
+      ),
+    );
   }
 }
