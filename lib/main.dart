6a52cf74426533186071e37a8fb0a33ceb71c89f
Vik1ang <vik1ang@outlook.com>
2022-05-19 11:14:04 +0800

    dart study

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 
 main() {
   // 1. runApp函数
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HYHomePage());
+  }
+}
+
+class HYHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text("Flutter App"),
         ),
-        body: const Center(
-            child: Text(
-          "Hello World",
-          style: TextStyle(fontSize: 30, color: Colors.orange),
-        ))),
-  ));
+        body: HYContentBody());
+  }
+}
+
+class HYContentBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text(
+      "Hello World",
+      style: TextStyle(fontSize: 30, color: Colors.orange),
+    ));
+  }
 }
