987e271f8fb655dbdcf8d84c705a5e6cc59e7366
Vik1ang <vik1ang@outlook.com>
2022-05-19 11:05:59 +0800

    dart study

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 
 main() {
   // 1. runApp函数
-  runApp(
-    Center(
-      child: ,
-    )
-      Text(
-    "Hello World",
-    textDirection: TextDirection.ltr,
-    style: TextStyle(
-      fontSize: 30,
-      color: Colors.orange
-    ),
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter App"),
+        ),
+        body: const Center(
+            child: Text(
+          "Hello World",
+          style: TextStyle(fontSize: 30, color: Colors.orange),
+        ))),
   ));
 }
